edb2591cfb373acda76e482f86e5c8d1045e3358
Anna Maniuk <anna@maniuk.nyc>
2023-09-21 20:04:02 +0000

    Finished all source code

--- simple.c
+++ simple.c
@@ -1,24 +1,34 @@
-#include </usr/src/kernels/6.1.41-63.114.amzn2023.x86_64/include/linux/init.h>
-#include </usr/src/kernels/6.1.41-63.114.amzn2023.x86_64/include/linux/kernel.h>
-#include </usr/src/kernels/6.1.41-63.114.amzn2023.x86_64/include/linux/module.h>
+/**
+ *  * simple.c
+ *   *
+ *    * A simple kernel module. 
+ *     * 
+ *      * To compile, run makefile by entering "make"
+ *       *
+ *        * Operating System Concepts - 10th Edition
+ *         * Copyright John Wiley & Sons - 2018
+ *          */
 
-/* This function is called when the module is loaded. */ 
-int simple_init(void)
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+/* This function is called when the module is loaded. */
+static int simple_init(void)
 {
-  printk(KERN_INFO "Loading Kernel Module\n");
+	       printk(KERN_INFO "Loading Module\n");
 
-  return 0;
+	              return 0;
 }
 
-/* This function is called when the module is removed. */ 
-void simple_exit(void)
-{
-  printk(KERN_INFO "Removing Kernel Module∖n");
+/* This function is called when the module is removed. */
+static void simple_exit(void) {
+		printk(KERN_INFO "Removing Module\n");
 }
 
 /* Macros for registering module entry and exit points. */
-module_init(simple_init);
-module_exit(simple_exit);
+module_init( simple_init );
+module_exit( simple_exit );
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Simple Module");
