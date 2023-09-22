ca7296219133b6d0078565f8992edfe8f5bde98c
Anna Maniuk <anna@maniuk.nyc>
2023-09-22 21:42:24 +0000

    Completed exersices with simple.c

--- simple.c
+++ simple.c
@@ -1,29 +1,38 @@
 /**
- *  * simple.c
- *   *
- *    * A simple kernel module. 
- *     * 
- *      * To compile, run makefile by entering "make"
- *       *
- *        * Operating System Concepts - 10th Edition
- *         * Copyright John Wiley & Sons - 2018
- *          */
+ * simple.c
+ * A simple kernel module. 
+ * To compile, run makefile by entering "make"
+ * Operating System Concepts - 10th Edition
+ * Copyright John Wiley & Sons - 2018
+ */
 
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/hash.h>
+#include <linux/gcd.h>
+#include <asm/param.h>
+#include <linux/jiffies.h>
 
 /* This function is called when the module is loaded. */
 static int simple_init(void)
 {
-	       printk(KERN_INFO "Loading Module\n");
-
-	              return 0;
+	printk(KERN_INFO "Loading Module\n");
+	printk(KERN_INFO "%lu\n", GOLDEN_RATIO_PRIME);
+	printk(KERN_INFO "Jiffies = %d\n", jiffies);
+	printk(KERN_INFO "HZ = %d\n", HZ);
+	printk(KERN_INFO "Loaded Module\n");
+	return 0;
 }
 
 /* This function is called when the module is removed. */
-static void simple_exit(void) {
-		printk(KERN_INFO "Removing Module\n");
+static void simple_exit(void)
+{
+	printk(KERN_INFO "Removing Module\n");
+	unsigned long result = gcd(3300, 24);
+	printk(KERN_INFO "%lu\n", result);
+	printk(KERN_INFO "Jiffies = %d\n", jiffies);
+	printk(KERN_INFO "Removed Module\n");
 }
 
 /* Macros for registering module entry and exit points. */
